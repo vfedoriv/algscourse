117453996a0bbadc5d6c9fd3d404740850814c71
Vitaliy Fedoriv <vitaliy.fedoriv@gmail.com>
2018-03-23 15:54:51 +0200

    some methods realization

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -5,47 +5,101 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 	private int[][] grid;
+	private int n;
+	private int[] openArray;
+	private int openSites = 0;
+	private WeightedQuickUnionUF wQUF;
+	
+	private int xyTo1D(int row, int col) {
+		// check row col bounds
+		System.out.println("xyTo1D: " + (row*n + col + 1));
+		return row*n + col + 1;
+	};
 	
 	public Percolation(int n) {
+		// add int overflow check
+		// n > (Integer.MAX_VALUE-1)/2
+		this.n = n;
 		grid = new int[n][n];
-		int cell_value = 1;
+		openArray = new int[n*n];
 		for (int i=0; i < n ; i++) {
-			for (int j=0; j<n; j++, cell_value++) {
-				// grid[i][j]=i*10 +j +1;
-				grid[i][j]=cell_value;
+			for (int j=0; j<n; j++) {
+				grid[i][j]= i*n + j + 1;
 			}
+		}
+		wQUF = new WeightedQuickUnionUF(n*n);
+	};
+	
+	public void open(int row, int col) {
+		int i = 0;
+		if (isFull(row, col)) {
+			System.out.println("open: row: " + row + " col : " + col );
+			i = xyTo1D(row,col);
+			openArray[i-1] = i;
+			openSites++;
+		if  ((i > 0) && (openArray[i] > 0)) wQUF.union(i-1, i);
+		if  (((i-2) > 0) && (openArray[i-2] > 0))
+			wQUF.union(i-1, i-2);
+		if  (((i-n-1) > 0) && (openArray[i-n-1] > 0)) 
+			wQUF.union(i-1, i-n-1);
+		if  (((i+n-1) < n*n) && (openArray[i] > 0)) 
+			wQUF.union(i-1, i+n-1);
+		
+		
 		}
 		
 		
 	};
 	
-	public void open(int row, int col) {
-		
-	};
-	
 	public boolean isOpen(int row, int col) {
+		if (openArray[xyTo1D(row,col)] > 0) return true;
 		return false;
 	};
 	
 	public boolean isFull(int row, int col) {
+		if (openArray[xyTo1D(row,col)] == 0) return true;
 		return false;
 	};
 	
 	public int numberOfOpenSites() {
-		return 0;
+		return this.openSites;
 	};
 	
 	public boolean percolates() {
 		return false;
 	};
 	
+	public boolean conected() {
+		System.out.println(wQUF.connected(xyTo1D(0,0), xyTo1D(0,1)));
+		System.out.println(wQUF.connected(xyTo1D(0,1), xyTo1D(1,1)));
+		System.out.println(wQUF.connected(xyTo1D(1,1), xyTo1D(1,2)));
+		System.out.println(wQUF.connected(xyTo1D(1,2), xyTo1D(2,1)));
+		System.out.println(wQUF.connected(xyTo1D(1,2), xyTo1D(2,2)));
+		System.out.println(wQUF.connected(xyTo1D(2,1), xyTo1D(3,1)));
+		System.out.println(wQUF.connected(xyTo1D(2,2), xyTo1D(3,1)));
+		System.out.println(wQUF.connected(xyTo1D(2,2), xyTo1D(3,2)));
+		System.out.println(wQUF.connected(xyTo1D(2,2), xyTo1D(3,3)));
+		System.out.println(wQUF.connected(xyTo1D(2,3), xyTo1D(3,3)));
+		return wQUF.connected(xyTo1D(0,0), xyTo1D(2,2));
+		
+	};
+	
 	public static void main(String[] args) {
-		Percolation perc = new Percolation(10);
-		for (int i=0; i < 10 ; i++) {
-			for (int j=0; j<10; j++) {
-				StdOut.println(perc.grid[i][j]);
-			}
-		}
+		int n = 10;
+		Percolation perc = new Percolation(n);
+		perc.open(0, 0);
+		perc.open(1, 1);
+		perc.open(2, 2);
+		perc.open(3, 3);
+		perc.open(0, 1);
+		perc.open(1, 2);
+		perc.open(2, 3);
+		System.out.println(perc.conected());
+//		for (int i=0; i < n ; i++) {
+//			for (int j=0; j<n; j++) {
+//				StdOut.println(perc.grid[i][j]);
+//			}
+//		}
 		
 		
 	};
--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -1,10 +1,30 @@
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class PercolationStats {
 	
+	private Percolation perc;
+	private int n;
+	int[] array1;
+	
+	
+	private int getRandomCell(int row, int col) {
+		return 0;
+		
+	};
+	
 	public PercolationStats(int n, int trials) {
+		array1 = new int[n*2];
+		int row, col;
+		perc = new Percolation(n);
+		while (!perc.percolates()) {
+			row = StdRandom.uniform(1,n);
+			col = StdRandom.uniform(1,n);
+			if (!perc.isFull(row, col)) perc.open(row, col);
+		}
+		StdOut.println("open sites: " + perc.numberOfOpenSites());
 		
 	};
 	
