61b0432184c694572430e33dfe2d11a727ec5237
Vitaliy Fedoriv <vitaliy.fedoriv@gmail.com>
2018-03-24 18:26:54 +0200

    work (integer array)

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,143 +1,98 @@
-import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
+// import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 	private int[][] grid;
-<<<<<<< .merge_file_a01996
-	private int grid_size;
-	
-	private boolean inRange(int row, int col) {
-		if ((row <=0) || (row > grid_size)) throw new IllegalArgumentException("index out of bounds; expected: 0 < row <= " + grid_size + "; actual: " + row);
-		if ((col <=0) || (col > grid_size)) throw new IllegalArgumentException("index out of bounds; expected: 0 < col <= " + grid_size + "; actual: " + col);
-		return true;
-	}
-	
-	public Percolation(int n) {
-		
-		if (!(n > 0)) throw new IllegalArgumentException("expected: argument should be > 0; actual: " + n);
-		grid_size = n;
-=======
 	private int n;
-	private int[] openArray;
+	private int[] openArray; // as variant - realization with boolean array can save some memory
 	private int openSites = 0;
 	private WeightedQuickUnionUF wQUF;
-	
+
+	private boolean inBound(int row, int col) {
+		if ((row <= 0) || (row > n))
+			throw new IllegalArgumentException("index out of bounds; expected: 0 < row <= " + n + "; actual: " + row);
+		if ((col <= 0) || (col > n))
+			throw new IllegalArgumentException("index out of bounds; expected: 0 < col <= " + n + "; actual: " + col);
+		return true;
+	}
+
 	private int xyTo1D(int row, int col) {
-		// check row col bounds
-		System.out.println("xyTo1D: " + (row*n + col + 1));
-		return row*n + col + 1;
+		inBound(row, col);
+		return (row - 1) * n + col;
 	};
-	
+
 	public Percolation(int n) {
-		// add int overflow check
-		// n > (Integer.MAX_VALUE-1)/2
+		if (!(n > 0) || (n > 4096)) // max size depends of java heap size
+			throw new IllegalArgumentException("expected: argument should be between 1 and 4096; actual: " + n);
 		this.n = n;
->>>>>>> .merge_file_a00992
+
+		openArray = new int[n * n + 2];
+		openArray[0] = 0; // virtual top site
+		openArray[n * n + 1] = n * n + 1; // virtual bottom site
+
 		grid = new int[n][n];
-		openArray = new int[n*n];
-		for (int i=0; i < n ; i++) {
-			for (int j=0; j<n; j++) {
-				grid[i][j]= i*n + j + 1;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				grid[i][j] = i * n + j + 1;
 			}
 		}
-		wQUF = new WeightedQuickUnionUF(n*n);
+		// first element - virtual top site, last - virtual bottom site
+		wQUF = new WeightedQuickUnionUF(n * n + 2);
 	};
-	
+
 	public void open(int row, int col) {
-<<<<<<< .merge_file_a01996
-		inRange(row, col);
-=======
+		inBound(row, col);
 		int i = 0;
 		if (isFull(row, col)) {
-			System.out.println("open: row: " + row + " col : " + col );
-			i = xyTo1D(row,col);
-			openArray[i-1] = i;
+//			System.out.println("open: row: " + row + " col : " + col);
+			i = xyTo1D(row, col);
+			openArray[i] = i;
 			openSites++;
-		if  ((i > 0) && (openArray[i] > 0)) wQUF.union(i-1, i);
-		if  (((i-2) > 0) && (openArray[i-2] > 0))
-			wQUF.union(i-1, i-2);
-		if  (((i-n-1) > 0) && (openArray[i-n-1] > 0)) 
-			wQUF.union(i-1, i-n-1);
-		if  (((i+n-1) < n*n) && (openArray[i] > 0)) 
-			wQUF.union(i-1, i+n-1);
-		
-		
+
+			if ((i > 1) && (openArray[i - 1] > 0))
+				wQUF.union(i - 1, i); // left neighbor
+
+			if ((i > 0) && (openArray[i + 1] > 0)) // right neighbor
+				wQUF.union(i + 1, i);
+
+			if (((i - n) > 0) && (openArray[i - n] > 0)) // top neighbor
+				wQUF.union(i - n, i);
+			else
+				if ((i - n) <= 0) wQUF.union(0, i); // connect to virtual top site
+
+			if (((i + n) < n * n) && (openArray[i + n] > 0)) // bottom neighbor
+				wQUF.union(i + n, i);
+			else
+				if ((i + n) >= n * n) wQUF.union(n * n + 1, i); // connect to virtual bottom site
 		}
-		
->>>>>>> .merge_file_a00992
-		
+
 	};
-	
+
 	public boolean isOpen(int row, int col) {
-<<<<<<< .merge_file_a01996
-		inRange(row, col);
-=======
-		if (openArray[xyTo1D(row,col)] > 0) return true;
->>>>>>> .merge_file_a00992
+		inBound(row, col);
+		if (openArray[xyTo1D(row, col)] > 0)
+			return true;
 		return false;
 	};
-	
+
 	public boolean isFull(int row, int col) {
-<<<<<<< .merge_file_a01996
-		inRange(row, col);
-=======
-		if (openArray[xyTo1D(row,col)] == 0) return true;
->>>>>>> .merge_file_a00992
+		inBound(row, col);
+		if (openArray[xyTo1D(row, col)] == 0)
+			return true;
 		return false;
 	};
-	
+
 	public int numberOfOpenSites() {
 		return this.openSites;
 	};
-	
+
 	public boolean percolates() {
+		if (wQUF.connected(0, n * n + 1))
+			return true;
 		return false;
 	};
-	
-	public boolean conected() {
-		System.out.println(wQUF.connected(xyTo1D(0,0), xyTo1D(0,1)));
-		System.out.println(wQUF.connected(xyTo1D(0,1), xyTo1D(1,1)));
-		System.out.println(wQUF.connected(xyTo1D(1,1), xyTo1D(1,2)));
-		System.out.println(wQUF.connected(xyTo1D(1,2), xyTo1D(2,1)));
-		System.out.println(wQUF.connected(xyTo1D(1,2), xyTo1D(2,2)));
-		System.out.println(wQUF.connected(xyTo1D(2,1), xyTo1D(3,1)));
-		System.out.println(wQUF.connected(xyTo1D(2,2), xyTo1D(3,1)));
-		System.out.println(wQUF.connected(xyTo1D(2,2), xyTo1D(3,2)));
-		System.out.println(wQUF.connected(xyTo1D(2,2), xyTo1D(3,3)));
-		System.out.println(wQUF.connected(xyTo1D(2,3), xyTo1D(3,3)));
-		return wQUF.connected(xyTo1D(0,0), xyTo1D(2,2));
-		
-	};
-	
-	public static void main(String[] args) {
-<<<<<<< .merge_file_a01996
-		Percolation perc = new Percolation(10);
-		for (int i=0; i < 10 ; i++) {
-			for (int j=0; j<10; j++) {
-				StdOut.println(perc.grid[i][j]);
-			}
-		}
-		perc.isFull(10, 11);
-=======
-		int n = 10;
-		Percolation perc = new Percolation(n);
-		perc.open(0, 0);
-		perc.open(1, 1);
-		perc.open(2, 2);
-		perc.open(3, 3);
-		perc.open(0, 1);
-		perc.open(1, 2);
-		perc.open(2, 3);
-		System.out.println(perc.conected());
-//		for (int i=0; i < n ; i++) {
-//			for (int j=0; j<n; j++) {
-//				StdOut.println(perc.grid[i][j]);
-//			}
-//		}
->>>>>>> .merge_file_a00992
-		
-		
-	};
+
+//	public static void main(String[] args) {
+//
+//	};
 }
--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -1,51 +1,60 @@
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
-import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class PercolationStats {
-	
+
 	private Percolation perc;
-	private int n;
-	int[] array1;
+	private int grid_size;
+	private int experiments_count;
+	double[] fractions;
 	
-	
-	private int getRandomCell(int row, int col) {
-		return 0;
-		
+	private void openRandomSite() {
+		int row = StdRandom.uniform(1,grid_size+1);
+		int col = StdRandom.uniform(1,grid_size+1);
+		if (perc.isFull(row, col)) perc.open(row, col);
 	};
 	
 	public PercolationStats(int n, int trials) {
-		array1 = new int[n*2];
-		int row, col;
-		perc = new Percolation(n);
-		while (!perc.percolates()) {
-			row = StdRandom.uniform(1,n);
-			col = StdRandom.uniform(1,n);
-			if (!perc.isFull(row, col)) perc.open(row, col);
-		}
-		StdOut.println("open sites: " + perc.numberOfOpenSites());
+		if ((n <= 0) || (trials <=0))
+			throw new IllegalArgumentException("n and trials should be > 0; " + " actual: n = " + n + " ; trials = " + trials);
+		grid_size = n;
+		experiments_count = trials;
+		fractions = new double[experiments_count];
 		
+		for(int count = 0; count < experiments_count; count++) {
+			perc = new Percolation(grid_size);
+			while (!perc.percolates()) {
+				openRandomSite();
+			}
+			fractions[count] = (double) perc.numberOfOpenSites()/(grid_size*grid_size);
+		}
 	};
 	
 	public double mean() {
-		return 0;
+		return StdStats.mean(fractions);
 	};
 	
 	public double stddev() {
-		return 0;
+		if (experiments_count == 1) return Double.NaN;
+		return StdStats.stddev(fractions);
 	};
 	
 	public double confidenceLo() {
-		return 0;
+		return mean() - 1.96*stddev()/Math.sqrt(experiments_count);
 	};
 	
 	public double confidenceHi() {
-		return 0;
+		return mean() + 1.96*stddev()/Math.sqrt(experiments_count);
 	};
 	
 	public static void main(String[] args) {
-		
+		int n = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
+        PercolationStats percStats = new PercolationStats(n, T);
+        StdOut.println("mean                    = " + percStats.mean());
+        StdOut.println("stddev                  = " + percStats.stddev());
+        StdOut.println("95% confidence interval = [" + percStats.confidenceLo() + ", " + percStats.confidenceHi() + "]");
 	};
 		
 }
