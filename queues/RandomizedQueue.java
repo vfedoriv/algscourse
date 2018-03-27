305dcfc2eae89c00c3785c30dea6e7a54fe32631
Vitaliy Fedoriv <vitaliy.fedoriv@gmail.com>
2018-03-27 14:52:32 +0300

    add RandomizedQueue

---
+++ queues/RandomizedQueue.java
@@ -0,0 +1,47 @@
+//public class RandomizedQueue<Item> implements Iterable<Item> {
+//   public RandomizedQueue()                 // construct an empty randomized queue
+//   public boolean isEmpty()                 // is the randomized queue empty?
+//   public int size()                        // return the number of items on the randomized queue
+//   public void enqueue(Item item)           // add the item
+//   public Item dequeue()                    // remove and return a random item
+//   public Item sample()                     // return a random item (but do not remove it)
+//   public Iterator<Item> iterator()         // return an independent iterator over items in random order
+//   public static void main(String[] args)   // unit testing (optional)
+//}
+import edu.princeton.cs.algs4.StdOut;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+	public RandomizedQueue() {
+		
+	}
+	
+	public boolean isEmpty() {
+		return false;
+	}
+	
+	public int size() {
+		return 0;
+	}
+	
+	public void enqueue(Item item) {
+		
+	}
+	
+	public Item dequeue() {
+		return null;
+	}
+	
+	public Item sample() {
+		return null;
+	}
+	
+	public Iterator<Item> iterator() {
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
