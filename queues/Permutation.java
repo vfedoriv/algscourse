529ea3d21ea15366dd0a8c34433f4de13ce6471e
Vitaliy Fedoriv <vitaliy.fedoriv@gmail.com>
2018-03-28 14:13:45 +0300

    add Permutation implementation

--- queues/Permutation.java
+++ queues/Permutation.java
@@ -1,5 +1,20 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdIn;
+
 public class Permutation {
 	public static void main(String[] args) {
+		RandomizedQueue<String> rq = new RandomizedQueue<String>();
+		int k = Integer.parseInt(args[0]);
+		while (!StdIn.isEmpty()) {
+			String current = StdIn.readString();
+			rq.enqueue(current);
+		}
 		
+		int i = 1;
+		for (String str : rq) {
+			if (i > k) break;
+			StdOut.println(str);
+			i++;
+		}
 	}
 }
